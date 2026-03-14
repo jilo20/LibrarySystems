3953da566955376d23bff2fa5ece0ac3be71a2a7
jilo20 <angelobazar20@gmail.com>
2026-03-14 23:57:51 +0800

    Understand fetching data

--- frontend/src/components/BookPage.jsx
+++ frontend/src/components/BookPage.jsx
@@ -11,14 +11,27 @@ function BookPage() {
 }
 
 export function BorrowBook() {
+    //states for inputs
+    const [bookId, setBookId] = useState('');
+    const [studentId, setStudentId] = useState('');
     const transactions = useTransactions();
-    console.log(transactions);
 
-    transactions.forEach(tr => {
-        if(tr.book == 1){
-            console.log('Transaction BookID:  ',tr.book)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const currentDate = new Date().toISOString().split('T')[0];
+
+        if(transactions.length === 0) console.log('transactions nai');
+        const isBorrowed = transactions.some(
+            (transact) => transact.book === bookId
+        );
+
+        if(!isBorrowed){
+            console.log('BookID: ',bookId,'\nStudentID: ',studentId);
+            fetch('http://localhost:8000/api/transactions')
         }
-    });
+        else
+            alert('Already Borrowed');
+    }
 
     return (
         <div className="w-full min-h-screen flex justify-center pt-30">
@@ -28,13 +41,14 @@ export function BorrowBook() {
                     animation={false}
                 >
                     <p className="text-[2em] font-bold">Borrow Book</p>
-                    <form className="flex flex-col gap-2">
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
                         <p className="text-gray-800 text-md font-semibold">Student ID</p>
                         <div className="focus:scale-103">
                             <input
                                 type="text"
-                                name="studentId"
                                 className="font-light smooth-avg border border-gray-300 border-b-4 border-b-green-700 rounded-sm text-lg p-2 w-[20em]"
+                                value={studentId}
+                                onChange={(e)=>setStudentId(e.target.value)}
                             />
                         </div>
 
@@ -42,8 +56,9 @@ export function BorrowBook() {
                         <div className="focus:scale-103 mb-6">
                             <input
                                 type="text"
-                                name="bookId"
                                 className="font-light smooth-avg border border-gray-300 border-b-4 border-b-green-700 rounded-sm text-lg p-2 w-[20em]"
+                                value={bookId}
+                                onChange={(e)=>setBookId(e.target.value)}
                             />
                         </div>
 
